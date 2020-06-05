751420b92ab5626ab74cc4bb3f65c490752e3760
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2020-06-05 10:24:42 +0300

    Fetching and validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,36 +1,55 @@
 // ---- 1.SELECTING ELEMENTS ----
-const addMovieModal = document.getElementById('add-modal')
+
+const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.querySelector('#add-modal')
 // const addMovieModal = document.body.children[1]
-const startMovieButton = document.querySelector('header button')
+const startMovieButton = document.querySelector('header button');
 // const startMovieButton = document.querySelector('header').lastElementChild
-const backdrop = document.getElementById('backdrop')
+const backdrop = document.getElementById('backdrop');
 // const backdrop = document.body.firstElementChild
-const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
-
-
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
+// const userInputs = addMovieModal.getElementsByTagName('input');
 
 // ---- 2.ADDING EVENT LISTENERS ----
 
 // 2.3. utilities, used for reusability purpose
 const toggleBackdrop = () => {
-    backdrop.classList.toggle('visible')
-}
+  backdrop.classList.toggle('visible');
+};
 
 // 2.2. functions that goes directly into the event listeners
 const toggleMovieModal = () => {
-    addMovieModal.classList.toggle('visible')
-    toggleBackdrop();
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
 };
 const backtodropClickHandler = () => {
-    toggleMovieModal()
-}
-const cancelAddMovie = () => {
-    toggleMovieModal()
-}
+  toggleMovieModal();
+};
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
 
+  // basic check
+  if (
+    titleValue.trim() === '' ||
+    imageValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 1
+  ) {
+    alert('Please enter valid values (rating between 1 and 5)');
+    return;
+  }
+};
 
 // 2.1. register event listeners
-startMovieButton.addEventListener('click', toggleMovieModal)
-backdrop.addEventListener('click', backtodropClickHandler)
-cancelAddMovieButton.addEventListener('click', cancelAddMovie)
+startMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backtodropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
