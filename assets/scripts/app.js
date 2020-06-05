66910ad327d7404dab7d82eed3317504d37d0728
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2020-06-05 09:40:24 +0300

    Adding or removing the back drop and implement cancel functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,14 +2,35 @@
 const addMovieModal = document.getElementById('add-modal')
 // const addMovieModal = document.querySelector('#add-modal')
 // const addMovieModal = document.body.children[1]
-
 const startMovieButton = document.querySelector('header button')
 // const startMovieButton = document.querySelector('header').lastElementChild
+const backdrop = document.getElementById('backdrop')
+// const backdrop = document.body.firstElementChild
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
+
 
 
 // ---- 2.ADDING EVENT LISTENERS ----
+
+// 2.3. utilities, used for reusability purpose
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible')
+}
+
+// 2.2. functions that goes directly into the event listeners
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible')
+    toggleBackdrop();
 };
+const backtodropClickHandler = () => {
+    toggleMovieModal()
+}
+const cancelAddMovie = () => {
+    toggleMovieModal()
+}
 
+
+// 2.1. register event listeners
 startMovieButton.addEventListener('click', toggleMovieModal)
+backdrop.addEventListener('click', backtodropClickHandler)
+cancelAddMovieButton.addEventListener('click', cancelAddMovie)
