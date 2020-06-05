6537fcebba5bd181e15b3fc192a26f9ea2e9d149
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2020-06-05 09:19:10 +0300

    Accessing the modal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,15 @@
+// ---- 1.SELECTING ELEMENTS ----
 const addMovieModal = document.getElementById('add-modal')
 // const addMovieModal = document.querySelector('#add-modal')
 // const addMovieModal = document.body.children[1]
 
 const startMovieButton = document.querySelector('header button')
 // const startMovieButton = document.querySelector('header').lastElementChild
+
+
+// ---- 2.ADDING EVENT LISTENERS ----
+const toggleMovieModal = () => {
+    addMovieModal.classList.toggle('visible')
+};
+
+startMovieButton.addEventListener('click', toggleMovieModal)
