84ee33749c276ff2fcb1b23d0b3519399e2e2e34
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2020-06-05 09:09:01 +0300

    Selecting Modal and Button

---
+++ assets/scripts/app.js
@@ -0,0 +1,6 @@
+const addMovieModal = document.getElementById('add-modal')
+// const addMovieModal = document.querySelector('#add-modal')
+// const addMovieModal = document.body.children[1]
+
+const startMovieButton = document.querySelector('header button')
+// const startMovieButton = document.querySelector('header').lastElementChild
