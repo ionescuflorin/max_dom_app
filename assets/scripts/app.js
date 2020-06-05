3e9dd46f7428bb1a8745298e9a76f03a126e2ae9
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2020-06-05 10:35:43 +0300

    Creating a movie model and clearing the inputs after submitting

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,12 +12,23 @@ const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 // const userInputs = addMovieModal.getElementsByTagName('input');
 
+// ---- 3. FUNCTIONALITY LOGIC ------
+
+// 3.1. declaring state
+const movies = [];
+
 // ---- 2.ADDING EVENT LISTENERS ----
 
 // 2.3. utilities, used for reusability purpose
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
 };
+
+const clearMovieInput = () => {
+  for (const userInput of userInputs) {
+      userInput.value = '';
+  }
+}
 
 // 2.2. functions that goes directly into the event listeners
 const toggleMovieModal = () => {
@@ -29,6 +40,7 @@ const backtodropClickHandler = () => {
 };
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
 };
 const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
@@ -41,11 +53,26 @@ const addMovieHandler = () => {
     imageValue.trim() === '' ||
     ratingValue.trim() === '' ||
     +ratingValue < 1 ||
-    +ratingValue > 1
+    +ratingValue > 5
   ) {
     alert('Please enter valid values (rating between 1 and 5)');
     return;
   }
+
+  // creating the new movie model
+  const newMovie = {
+    title: titleValue,
+    image: imageValue,
+    rating: ratingValue,
+  };
+  // push it into the state
+  movies.push(newMovie);
+  console.log(movies);
+  
+  // close the modal
+  toggleMovieModal();
+  // clear inputs
+  clearMovieInput();
 };
 
 // 2.1. register event listeners
